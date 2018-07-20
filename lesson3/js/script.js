9d2cad7ba08b49a46aa157033157a459572c9d30
greforce <greforce76@gmail.com>
2018-07-20 17:49:27 +0300

    lesson3 - 2 варианта gq: -требующий init() / gq, и без него / gq2

--- lesson3/js/script.js
+++ lesson3/js/script.js
@@ -1,16 +1,16 @@
 window.onload = function() {
-  // gq.init().get('^p').get('.elem2').setAttr('name', 'name5');
-  // gq.init().get('.elem2').addClass('myclass1');
-  // gq.init().get('#test2').setAttr('name', 'nnn3');
-  // gq.init().get('+nnn3').addClass('a123').addClass('abc1234');
+  gq.init().get('^p').get('.elem2').setAttr('name', 'name5');
+  gq.init().get('.elem2').addClass('myclass1');
+  gq.init().get('#test2').setAttr('name', 'nnn3');
+  gq.init().get('+nnn3').addClass('a123').addClass('abc1234');
 
 
 
-  gq().get('^p').get('.elem2').setAttr('name', 'name5');
-  gq().get('.elem2').addClass('myclass1');
-  gq().get('#test2').setAttr('name', 'nnn3');
-  gq().get('+nnn3').addClass('a123').addClass('abc1234');
+  gq2().get('^p').get('.elem2').setAttr('name', 'name5');
+  gq2().get('.elem2').addClass('myclass1');
+  gq2().get('#test2').setAttr('name', 'nnn3');
+  gq2().get('+nnn3').addClass('a123').addClass('abc1234');
 
-  gq().get('.elem2').setText('fff');
+  gq2().get('.elem2').setText('fff');
 
 }
